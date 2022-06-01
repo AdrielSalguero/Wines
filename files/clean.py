c4d9d57265a8dc34233fb6f6d5604b6d4f3b0495
asalguero <adrielsalguero8@gmail.com>
2022-06-01 04:16:39 -0300

    Nuevas configuraciones posibles + comienzo de codigo para graficar con sns

--- files/clean.py
+++ files/clean.py
@@ -1,9 +1,8 @@
-from audioop import avg
-from cProfile import run
-from itertools import count
+from matplotlib.pyplot import axes
 import pandas as pd
 import numpy as np 
-import seaborn
+import seaborn as sns
+import statistics
 
 def shapes(df1, df2):
 
@@ -23,25 +22,47 @@ def shapes(df1, df2):
     return wine_df
 
 
-def pricepoints(dataframe):
+def modules(dataframe,category):
 
+    cat = str(category)
+    categories = dataframe[cat].unique().tolist()
+    for i in categories:
+
+       print(i)
+
+
+def pricepoints(dataframe,category):
+
+    cat = str(category)
     dataframe['R price/points'] = dataframe.price/dataframe.points
-    df_pricepoints = dataframe[['country', 'winery','variety','R price/points']]
-    df_pricepoints = df_pricepoints.groupby('country')['R price/points'].agg(max)
-    print('Precio calidad matriz \n', df_pricepoints)
-    return(dataframe)
+    df_pricepoints = dataframe[['country', 'winery','region_1','variety','R price/points']]
+    if cat =='variety':
+        df_pricepoints = (df_pricepoints.groupby(by= cat)[['winery','R price/points']]).max()
+    else:
+        df_pricepoints = (df_pricepoints.groupby(by = cat)[['winery','R price/points']]).max()
+    
+    
+    df_pricepoints = df_pricepoints.dropna(axis = 0, how='any')
+    return(df_pricepoints)
+
 
 def resume(dataframe, category):
 
     cat = str(category)
-    pointsdf = dataframe.groupby(cat)['points'].agg(max_points = 'max', min_points = 'min')
-    pointsdf ['delta points'] = ((pointsdf.max_points - pointsdf.min_points))
-    pricedf = dataframe.groupby(cat)['price'].agg(max_price = 'max', min_price = 'min')
-    pricedf ['delta price'] = ((pricedf.max_price - pricedf.min_price))
+    pointsdf = dataframe.groupby(cat)['points'].agg(MxPts = 'max', MnPts = 'min',AVGPts = 'mean').round(1)
+    pointsdf ['DTA Pts'] = ((pointsdf.MxPts - pointsdf.MnPts))
+    pricedf = dataframe.groupby(cat)['price'].agg(MxPri = 'max', MnPri = 'min', AVGPri = 'mean').round(1)
+    pricedf ['DTA price'] = ((pricedf.MxPri - pricedf.MnPri))
     number_of_test = dataframe.groupby(cat)['description'].count()
     compacta = pd.concat([pointsdf,pricedf, number_of_test], axis = 1)
+    compacta.rename({'description': 'tested'}, axis=1, inplace=True)
     compacta.fillna(0)
-    print(compacta)
+    #mask = compacta['tested'] > statistics.mean(compacta['tested'])*0.01
+    mask = compacta['tested'] > 50
+    filtered = compacta[mask]
+    return(filtered)
+
+
 
 
 def run ():
@@ -51,30 +72,29 @@ def run ():
     df_150 = pd.read_csv(r'C:\Users\Usuario\Documents\Codes\wine_analysis\data\winemag-data_first150k.csv')
 
     wines = shapes(df,df_150)
+    
     print('\n \n - - - - - - - - - - - - - - - -  - - - - - - - - - \n \n ')
-    pricepoints(wines)
     categories = {'1': 'country', '2':'variety', '3': 'region_1'}
     
 
     for i in categories:
-        print( '{} will be filter for {}'.format(i,categories[i]))
-
-
-    selector = input('Please insert your seleccion: ')
+        print( 'Insert {} if you want filter for {}'.format(i,categories[i]))
+    selector = input('\n \n Please insert your seleccion: ')
     category = categories[selector]
-    resume(wines,category)
-    #print(wines.head(10))
+    df_resume = resume(wines,category)
+    print(df_resume)
+    
 
+    question_1 = input('\n \n Insert - y - if you want see price point resume ')
+    if question_1 == 'y':
+
+        df_qualityprice = pricepoints(wines,category)
+        print(df_qualityprice)
+
+    plots(wines)
     #analitycs(wines)
 
-    #variety = wines['variety'].unique().tolist()
-    #countries = wines['country'].unique().tolist()
-    #region = wines['region_1'].unique().tolist()
-    #wineries = wines['winery'].unique().tolist()
 
-    #for i in variety:
-
-    #    print(i)
 
 
 
--- files/proofs.py
+++ files/proofs.py
@@ -1,7 +1,51 @@
+import statistics
+import matplotlib
+import pandas as pd
+import random
+import seaborn as sns
+
+
+
 dicc = {'1': 'Adriel', '2': 'Pablo' , '3': 'Juan Carlos'}
 
-ind = input('Please select your number : ')
+#ind = input('Please select your number : ')
 
-seleccion = dicc[ind]
-print(dicc)
-print(seleccion)
+#seleccion = dicc[ind]
+#print(dicc)
+#print(seleccion)
+
+
+
+average = []
+
+for i in range(20):
+
+    average.append(random.randint(0,52))
+
+#print(average)
+
+promedio = statistics.mean(average)
+#print(promedio)
+
+dfaveg = pd.DataFrame(average, columns=['Numeros'])
+
+dfavegs = dfaveg['Numeros']>statistics.mean(dfaveg['Numeros'])*0.2
+filtered = dfaveg[dfavegs]
+promedio = statistics.mean(filtered['Numeros'])
+#print(promedio)
+#print(filtered.shape)
+
+
+l = [[1, 2, 3], [1, None, 4], [2, 1, 3], [1, 2, 2]]
+df = pd.DataFrame(l, columns=["a", "b", "c"])
+#print(df)
+#print(df.groupby(by=["a"]).sum())
+#print(df.groupby(by=["b"]).sum())
+#print(df.groupby(by=["c"]).sum())
+
+df = pd.read_csv(r'C:\Users\Usuario\Documents\Codes\wine_analysis\data\winemag-data-130k-v2.csv')
+
+
+#sns.histplot(data = df, x = "country", hue= "points")
+sns.scatterplot(data = df, x='points', y= 'price', hue = 'country')
+matplotlib.pyplot.show()
